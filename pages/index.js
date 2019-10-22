9fd58eb30ceac2d7c24954ae11e07de21aadde85
Sirilada244Mai <sirilada.mai@gmail.com>
2019-10-22 14:03:53 +0700

    เพิ่ม เนื้อหา เเก้สีหัวข้อ

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,42 @@ const Home = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className="bd-example w-100 h-100 mt-5">
+                            <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
+                                <ol className="carousel-indicators">
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                                    <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                                </ol>
+                                <div className="carousel-inner">
+                                    <div className="carousel-item active">
+                                        <img src="./static/images/dog.jpg" className="d-block w-100" alt="..." />
+                                        <div className="carousel-caption d-none d-md-block">
+                                            <h5>First slide label</h5>
+                                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item">
+                                        <img src="./static/images/dog.jpg" className="d-block w-100" alt="..." />
+                                        <div className="carousel-caption d-none d-md-block">
+                                            <h5>Third slide label</h5>
+                                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Previous</span>
+                                </a>
+                                <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span className="sr-only">Next</span>
+                                </a>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </section>
         </Layout>
