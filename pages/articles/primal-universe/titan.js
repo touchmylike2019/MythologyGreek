e53a7301b31a7b02557be9521c862b501daba521
Sirilada244Mai <sirilada.mai@gmail.com>
2019-11-02 15:37:58 +0700

    ไททัน

--- pages/articles/primal-universe/titan.js
+++ pages/articles/primal-universe/titan.js
@@ -3,7 +3,82 @@ import DocumentsLayout from "../../../components/DocumentsLayout"
 const Titan = () => {
     return (
         <DocumentsLayout>
-            <h3 style={{marginTop: 130}}>ไททัน</h3>
+            <h3 style={{ marginTop: 130 }}>ไททัน</h3>
+
+
+            <h5 style={{ marginTop: 50 }}>โอเชียนัส </h5>
+            <p className="ml-3">(Oceanus or Okeanos เป็นบุตรคนโตในบรรดาพี่น้องสิบสองตน
+เป็นไททันแห่งทะเล และทุก ๆ น่านน้ำ ชอบอยู่กับปฐมเทพแห่งน้ำ "พอนตัส" ณ ส่วนลึกของทะเล อาวุธคือ ตรีศูล (Trident) ภริยาคือ เทธีส (Tethys)(เป็นน้องสาว)
+ มีบุตรสองคน คือ 1) Okeanides และ 2) Potamoi</p>
+            <img className="img-doc" src="/static/images/titan/1.jpg" alt="1" />
+
+            <h5 style={{ marginTop: 50 }}>โคออส</h5>
+            <p className="ml-3">เป็นเทพทางทิศเหนือ (North Pole) ไททันแห่งความห่างไกล สายตา (Far-sight) การแก้ไข (Resolve) และสติปัญญา (Intelligence) 
+            ต่อมาแต่งงานกับน้องสาว "ฟีบี (Phoebe or Phoibe)"  คำว่า Koios เป็นรากศัพท์ของคำว่า "query, questioning"
+             เป็นบิดาของ  "เลลันทอส (Lelantos)" "ลีโต (Leto)" และ "อัสเตเรีย (Asteria)" </p>
+            <img className="img-doc" src="/static/images/titan/2.jpg" alt="2" />
+
+
+            <h5 style={{ marginTop: 50 }}>ไครอัส </h5>
+            <p className="ml-3">เป็นไททันที่อยู่ทิศใต้ ไครอัสมีชื่อในระบบจักรราศีว่า "The Ram" เป็นเทพแห่งนักษัตร (Constellations) เป็นช่วงฤดูใบไม้ผลิมาจากทางทิศใต้
+             เป็นจุดเริ่มปีของปีปฏิทินกรีก บุตรชายของไครอัสคือ Astraios หรือ Astraeus เป็นเทพแห่งดวงดาว และภริยาคือ Eurybia เป็นบุตรสาวแห่งท้องทะเล 
+             ไครอัส มีถิ่นพำนักอยู่ในทาร์ทารัส (Tartarus) </p>
+            <img className="img-doc" src="/static/images/titan/3.jpg" alt="3" />
+
+            <h5 style={{ marginTop: 50 }}>ไฮเพอเรียน</h5>
+            <p className="ml-3">เป็นไททันทางทิศตะวันออก ไททันแห่งแสง ไฟ พระอาทิตย์ พระจันทร์ และรุ่งอรุณ สัญลักษณ์คือ ดวงอาทิตย์ (The Sun)
+             อาวุธเป็นไฟ และแสง ภริยาคือ เธเอีย (Theia) (เป็นน้องสาวด้วย) มีบุตรสามคน คือ 1) เฮลิออส (Helios 2) อีออส (Eos) และ 3) ซีลีนี (Selene)</p>
+            <img className="img-doc" src="/static/images/titan/4.png" alt="4" />
+
+            <h5 style={{ marginTop: 50 }}>ไอแอพิตัส  </h5>
+            <p className="ml-3">เป็นไททันทางทิศตะวันตก ไททันแห่งความตาย (Titan of Mortality) ความเจ็บปวด (Pain) และความตายที่รุนแรง (Violent Death) 
+            สัญลักษณ์คือ หอก (Spear) แต่งงานกับ Klymene-Asie มีบุตรห้าคน คือ 1) Atlas (ชาย) 2) Prometheus (ชาย) 3) Epimetheus (ชาย) 4) Menoitios (ชาย) และ 5) Ankhiale (หญิง)</p>
+            <img className="img-doc" src="/static/images/titan/5.jpg" alt="5" />
+
+            <h5 style={{ marginTop: 50 }}>เธเอีย </h5>
+            <p className="ml-3">เป็นไททันหญิง (Titaness) แห่งสายตาและการส่องแสง (Sight and Shining) ภริยาของไฮเพอเรียน เป็นมารดาของพระอาทิตย์ 
+            พระจันทร์ และรุ่งอรุณ มีบุตรสามคน คือ 1) เฮลิออส (Helios) 2) อีออส (Eos) และ 3) ซีลีนี (Selene) </p>
+            <img className="img-doc" src="/static/images/titan/6.jpg" alt="6" />
+
+
+            <h5 style={{ marginTop: 50 }}>เรอา </h5>
+            <p className="ml-3">เป็นไททันแห่งการแต่งงาน โลก ความอุดมสมบูรณ์ ภาวะเจริญพันธุ์ การสืบทอดรุ่นสู่รุ่น ความเป็นมารดา ความสะดวกสบาย 
+            (Titaness of Marriage, earth, fertility, generation, motherhood, comfort) เรอา แต่งงานกับ โครนอส ซึ่งเป็นน้องชาย ชื่อโรมันของเรอา 
+            คือ ไซเบล "Cybele" หรือ ออฟ "Ops". เรอามีบุตรกับโครนอส หกคน คือ 1) Hestia (หญิง) 2) Poseidon (ชาย) 3) Demeter (ชาย) 4) Hera (หญิง) 5) Hades (ชาย) และ 6) "ซูส  Zeus" (ชาย) </p>
+            <img className="img-doc" src="/static/images/titan/7.jpg" alt="7" />
+
+
+            <h5 style={{ marginTop: 50 }}>เธมีส </h5>
+            <p className="ml-3">เป็นไททันหญิงแห่งความยุติธรรม กฎหมาย และคำสั่ง (Titaness of justice, law and order) เธมีส เป็นภริยาตนที่สองของมหาเทพซูส (Zeus - King of Olympus)
+             เธอจะนั่งอยู่ถัดไปจากบัลลังก์ของซูส เพื่อให้คำแนะนำมหาเทพ เธอมีบุตรสององค์กับมหาเทพซูส คือ 1) The Moirai และ 2) the Horae.</p>
+            <img className="img-doc" src="/static/images/titan/8.jpg" alt="8" />
+
+
+            <h5 style={{ marginTop: 50 }}>นีโมไซน์ </h5>
+            <p className="ml-3">เป็นไททันหญิงแห่งความทรงจำ (Goddess of memory and remembrance) บ้างก็เป็นเทพด้านภาษา (Goddess of words and language) 
+            ซึ่งเธอประดิษฐ์ขึ้นมา เธอมีบุตรเก้าตน (ในกลุ่มที่เรียกว่า The Mousai หรือ The Muses) กับมหาเทพซูส (Zeus) ด้วยมหาเทพซูสร่วมหลับนอนกับเธอเก้าคืน ส่งผลให้เธอมีบุตรเก้าตน
+            ในเวลาต่อมา บุตรของเธอ ประกอบด้วย Kleio, Euterpe, Thaleia, Melpomeme, Terpsikhore, Erato, Polyhymnia, Ourania และ Kalliope</p>
+            <img className="img-doc" src="/static/images/titan/9.jpg" alt="9" />
+
+
+            <h5 style={{ marginTop: 50 }}>ฟีบี</h5>
+            <p className="ml-3">เป็นไททันแห่งความฉลาดปราดเปรื่อง (The Titanis of "Bright" intellect) เธอแต่งงานกับ โคออส 
+            (Coeus or Koios) มีบุตรด้วยกันสามตน คือ 1) "ลีโต (Leto)" 2) "อัสเตเรีย (Asteria)" และ 3) "เลลันทอส (Lelantos)"</p>
+            <img className="img-doc" src="/static/images/titan/10.jpg" alt="10" />
+
+            <h5 style={{ marginTop: 50 }}>ทีธิส</h5>
+            <p className="ml-3">เป็นภริยาและน้องสาวของ โอเชียนัส (Oceanus or Okeanos) ทั้งสองได้ร่วมกันสร้างแม่น้ำต่าง ๆ และท้องทะเลกว่าสามพันแห่ง มีบุตรด้วยกันสองตน คือ Oceanids และ Potamoi</p>
+            <img className="img-doc" src="/static/images/titan/11.jpg" alt="11" />
+
+
+            <h5 style={{ marginTop: 50 }}>โครนัส</h5>
+            <p className="ml-3">เป็นกษัตริย์แห่งไททัน เป็นบิดาของเทพโอลิมเปียนรุ่นแรก คือ Hestia, Demeter, Hera, Hades, Poseidon, และ Zeus 
+             โครนอสเป็นไททันแห่งจักรวาล (The Titan lord of the universe) โครนอส ปกครองในยุคที่เรียกว่าเป็น "ยุคทอง (Golden Age)" เขาเป็นไททันแห่งกาลเวลา 
+             การเก็บเกี่ยว โชคชะตา ความยุติธรรม และความชั่วร้าย โรมันเรียกเขาว่า แซทเทอร์น (Saturn) โดยมี เรอา (Rhea) เป็นภริยา</p>
+            <img className="img-doc" src="/static/images/titan/12.jpg" alt="12" />
+
+
+
         </DocumentsLayout>
     )
 }
