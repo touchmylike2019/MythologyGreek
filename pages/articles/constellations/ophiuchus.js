d0eb966bce729b175014a5af02f858abc9f89ea7
Sirilada244Mai <sirilada.mai@gmail.com>
2019-11-05 22:58:27 +0700

    ดาวเสร็จ

--- pages/articles/constellations/ophiuchus.js
+++ pages/articles/constellations/ophiuchus.js
@@ -3,7 +3,18 @@ import DocumentsLayout from '../../../components/DocumentsLayout'
 const Ophiuchus = () => {
     return (
         <DocumentsLayout>
-            <h3 style={{marginTop: 130}}>กลุ่มดาวคนแบกงู</h3>
+            <h3 style={{ marginTop: 130 }}>กลุ่มดาวคนแบกงู</h3>
+
+            <img className="img-doc" src="/static/images/constellations/Asclepius2.png" alt="Asclepius2" />
+
+
+            <p className="ml-3">  ตามตำนานกรีกเล่าว่า กลุ่มดาวนี้คือตัวแทนของแพทย์อัจฉริยะชาวกรีกนามว่า แอสคลีปิอุส (Asclepius) ซึ่งวันหนึ่งเข้าได้ล่วงรู้ความลับการทำให้ชีวิตเป็นอมตะจากการสุงเกตุสมุนไพรที่งูนำมากินเป็นประจำ เมื่อมหาเทพซุสได้รู้เข้า จึงทำการสังหาร แอสครีปิอุส ทันที
+             เพื่อไม่ให้ความลับของสวรรค์รั่วไหลและเพื่อรักษาสมดุลของโลกมนุษย์ไว้ จากนั้นซุสจึงนำดวงวิญญาณของ แอสครีปิอุส ไปประดับเป็นกลุ่มดาวคนแบกงูบนท้องฟ้า เพื่อไว้อาลัยและระลึกถึงเขา</p>
+            <img className="img-doc" src="/static/images/constellations/Asclepius.jpg" alt="Asclepius2" />
+
+        
+
+
         </DocumentsLayout>
     )
 }
