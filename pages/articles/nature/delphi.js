5d9092067d4950aac5c79c366a09b99963c63bba
Sirilada244Mai <sirilada.mai@gmail.com>
2019-11-06 15:47:16 +0700

    ธรรมมชาติ

--- pages/articles/nature/delphi.js
+++ pages/articles/nature/delphi.js
@@ -3,7 +3,20 @@ import DocumentsLayout from '../../../components/DocumentsLayout'
 const Delphi = () => {
     return (
         <DocumentsLayout>
-            <h3 style={{marginTop: 130}}>น้ำพุที่เดลฟี</h3>
+            <h3 style={{ marginTop: 130 }}>น้ำพุที่เดลฟี</h3>
+
+
+            <img className="img-doc" src="/static/images/nature/d.jpg" alt="d" />
+
+            <p className="ml-3">อพอลโลยังได้เคยไปหลงรักพรายน้ำนามว่า คาสทาเลีย (Castalia) คาสทาเลียไม่มีใจให้อพอลโลเธอจึงวิ่งหนีจนสุดท้ายเธอได้กระโจนหนีลงไปยังบ่อน้ำพุที่เดลฟี
+ น้ำพุนี้จึงถูกตั้งชื่อว่าคาสทาเลียตามเธอตั้งแต่นั้นมา เชื่อว่าน้ำพุบ่อนี้มีความศักดิ์สิทธิ์ น้ำจากน้ำพุจะถูกนำมาชำระล้างวิหารแห่งอพอลโลและมีสรรพคุณในการให้แรงบันดาลใจแก่นักกวี</p>
+
+
+
+
+
+
+
         </DocumentsLayout>
     )
 }
